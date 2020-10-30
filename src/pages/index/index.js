5768b9b1c795a3b8422a4d0cc94ec6e3b715c9e1
nineSean <ninesean1989@gmail.com>
2020-10-30 15:28:56 +0800

    fix: 修复路由异常

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import router from './router/index.js'
 import App from './index.vue'
 import entryConfig from "@util/entryConfig"
 
 entryConfig(Vue)
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
 
